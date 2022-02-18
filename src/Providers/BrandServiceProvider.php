658413e601b222c46bf07f551e924aa63631f65f
jabber <2898117012@qq.com>
2022-02-18 19:01:06 +0800

    init

--- src/Providers/BrandServiceProvider.php
+++ src/Providers/BrandServiceProvider.php
@@ -13,7 +13,7 @@ class BrandServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->mergeConfigFrom(__DIR__ . '/../../config/brand.php', 'brand');
     }
 
     /**
@@ -25,6 +25,21 @@ class BrandServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+
+            $this->publishes([
+                __DIR__ . '/../../config/brand.php' => config_path('brand.php')
+            ], 'config');
+
+            $this->publishes([
+                __DIR__ . '/../../resources/lang' => resource_path('lang/vendor/brand')
+            ], 'lang');
         }
+
+        $this->loadRoutesFrom(__DIR__ . '/../../routes/admin.php');
+        $this->loadRoutesFrom(__DIR__ . '/../../routes/seller.php');
+
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'brand');
+
+        $this->loadTranslationsFrom(__DIR__ . '/../../resources/lang', 'brand');
     }
 }
